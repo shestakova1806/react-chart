1469e108394a10c16c0a2f5605c12bfbb10fbb75
Alexandra Shestakova <shestakova18@bk.ru>
2020-11-16 19:19:39 +0300

    added profileInfod

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,13 @@
 import React, { useMemo, useState } from "react";
 import { Chart } from "../Chart/Chart";
 import { Legend } from "../Legend/Legend";
+import ProfilePhoto from "../../images/ProfilePhoto.png";
+import Pro from "../../images/Pro.png";
+import Frame from "../../images/Frame.png";
+import Path from "../../images/Path.png";
+import Writing from "../../images/Writing.png";
+import Person from "../../images/Person.png";
+import Connection from "../../images/Connection.png";
 import "./App.css";
 
 export const App = () => {
@@ -49,9 +56,48 @@ export const App = () => {
 
   return (
     <div className="app">
-      <div className="widget">
-        <Chart chartData={chartData} />
-        <Legend legends={legends} />
+      <div className="profile">
+        <div className="profileCard">
+          <div className="photo">
+            <img src={ProfilePhoto} alt="Photo" />
+          </div>
+          <div className="profileContent">
+            <span className="name">Петров Владимир Викторович</span>
+            <div className="profile-icons">
+              <img src={Pro} alt="Pro" />
+              <img src={Frame} alt="Frame" />
+            </div>
+            <span className="subheader">Роли на платформе</span>
+            <span className="content">Представитель компании</span>
+            <p className="content description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore ...
+            </p>
+          </div>
+        </div>
+        <div className="statistic">
+          <span className="subheader stat">Аналитика профиля </span>
+          <div className="profileAnalyse">
+            <div className="mentions">
+              <div className="cardContent">
+                <span className="cardHeader">Число упоминаний</span>
+                <span className="number">74</span>
+                <span className="content">Подпись текст</span>
+              </div>
+            </div>
+            <div className="requests">
+              <div className="cardContent">
+                <span className="cardHeader">Отвечает на запросы</span>
+                <span className="number">87%</span>
+                <span className="content">Санкт-Петербург, Россия</span>
+              </div>
+            </div>
+          </div>
+          <div className="widget">
+            <Chart chartData={chartData} />
+            <Legend legends={legends} />
+          </div>
+        </div>
       </div>
     </div>
   );
